f5dba9698498332550ef42ce0ef5890835ecef13
Samuel Tulach <samtulach@gmail.com>
2018-08-03 19:23:00 +0200

    Changed application version

--- VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Settings.Designer.cs
+++ VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Settings.Designer.cs
@@ -88,7 +88,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 26);
             this.label1.TabIndex = 1;
-            this.label1.Text = "VirusTotal Uploader\r\n0.0.1";
+            this.label1.Text = "VirusTotal Uploader\r\n0.0.2";
             // 
             // button1
             // 
