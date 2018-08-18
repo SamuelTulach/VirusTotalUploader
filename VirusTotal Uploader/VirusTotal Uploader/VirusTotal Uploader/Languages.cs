0e9c4eb919d0089de24dea162137855cc8559216
Samuel Tulach <samtulach@gmail.com>
2018-08-18 18:29:54 +0200

    Removed language system (Google Translate is slow)

--- VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Languages.cs
+++ VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Languages.cs
@@ -15,46 +15,12 @@ namespace VirusTotal_Uploader
 
         public void Init()
         {
-            try
-            {
-                client = TranslationClient.Create(null, TranslationModel.NeuralMachineTranslation); // TODO: Replace null with google cloud credentails
-            } catch (Exception error)
-            {
-                // No credentails
-            }
+            // This class is made for future languages support
         }
 
         public string GetString(string text)
         {
-            string language = "Original";
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "settings.txt"))
-            {
-                try
-                {
-                    string data = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "settings.txt");
-                    string[] arrayd = data.Split(':');
-                    language = arrayd[2];
-                } catch (Exception error)
-                {
-                    MessageBox.Show("Invalid settings file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            
-            if (language == "Original")
-            {
-                return text;
-            } else
-            {
-                try
-                {
-                    var response = client.TranslateText(text, language);
-                    return response.TranslatedText;
-                } catch
-                {
-                    // I love try catch
-                    return text;
-                }
-            }
+            return text;
         }
     }
 }
