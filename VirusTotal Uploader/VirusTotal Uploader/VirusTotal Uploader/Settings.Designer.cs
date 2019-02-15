250e3d654ae0cd5f9f8bea2342732b43734ed62b
Samuel Tulach <samtulach@gmail.com>
2019-02-15 17:31:47 +0100

    Updated version number

--- VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Settings.Designer.cs
+++ VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Settings.Designer.cs
@@ -136,7 +136,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 26);
             this.label1.TabIndex = 1;
-            this.label1.Text = "VirusTotal Uploader\r\n0.0.4";
+            this.label1.Text = "VirusTotal Uploader\r\n0.0.7";
             // 
             // button1
             // 
