38af00c99f9ae17cfe13d61e3b23f6f6d0b3e243
Sasi Karnati <sasi.karnati@thermofisher.com>
2020-03-12 12:53:22 -0700

    test

--- VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Program.cs
+++ VirusTotal Uploader/VirusTotal Uploader/VirusTotal Uploader/Program.cs
@@ -17,6 +17,7 @@ namespace VirusTotal_Uploader
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //test
         }
     }
 }
