08573382d683418e69399110dbf97ee5320bf301
Samuel Tulach <samtulach@gmail.com>
2023-02-19 10:57:31 +0100

    change settings save location since with new installer, the program files directory won't be writable

--- uploader/uploader/Settings.cs
+++ uploader/uploader/Settings.cs
@@ -16,7 +16,7 @@ namespace uploader
 
         public static string GetSettingsFilename()
         {
-            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vtu_settings.json");
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "vtu_settings.json");
         }
 
         public static void SaveSettings(Settings settings)
